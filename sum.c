ab1f5c2af75f12d95d209c1b980d52cbf7b640ca
Chitrashr <chitrashree036@gmail.com>
2024-10-03 03:55:11 +0530

    first commit

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,sum=0;
+    printf("\nEnter the first number:");
+    scanf("%d",&a);
+    printf("\nEnter the second number:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("\nThe sum = %d",sum);
+    return 0;
+}
