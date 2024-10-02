f37d20561668e9e9169d729b7e7263f1218ec389
Chitrashr <chitrashree036@gmail.com>
2024-10-03 04:48:04 +0530

    mul

---
+++ mul.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,mul=0;
+    printf("\nEnter the first number:");
+    scanf("%d",&a);
+    printf("\nEnter the second number:");
+    scanf("%d",&b);
+    mul=a*b;
+    printf("\nThe product = %d",mul);
+    return 0;
+}
