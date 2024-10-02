424173715c08d6703b3744eeca851e8f95d2323c
Chitrashr <chitrashree036@gmail.com>
2024-10-03 04:36:18 +0530

    subtract

---
+++ subtract.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sub=0;
+    printf("\nEnter the first number:");
+    scanf("%d",&a);
+    printf("\nEnter the second number:");
+    scanf("%d",&b);
+    sub=a-b;
+    printf("\nThe difference is %d",sub);
+    return 0;
+}
